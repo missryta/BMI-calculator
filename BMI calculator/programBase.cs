8a29514cd1f6c1d5b56f592b60378326746dd7ed
Ryta Esemene <Ryta Esemene@RYTA>
2023-02-15 20:52:20 +0100

    Add project files.

---
+++ BMI calculator/programBase.cs
@@ -0,0 +1,34 @@
+﻿namespace BMI_calculator
+{
+    internal class programBase
+    {
+        static void Main(string[] args)
+        {
+
+            //deklararer de varibler behövs
+            string namn, input;
+            int alder;
+            float langd, vikt;
+
+            //om namn
+            Console.WriteLine("Hej! Vad heter du?");
+            namn = Console.ReadLine();
+            Console.WriteLine("Hej! " + namn + ".");
+
+            //om ålder
+            Console.WriteLine("Hej!, Hur gammal är du?");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out alder);
+            Console.WriteLine("din alder är " + alder + ".");
+
+            //om längd
+            Console.WriteLine("Hur lång är du i cm?");
+            langd = float.Parse(Console.ReadLine());
+            Console.WriteLine("Din langd är " + langd + ".");
+
+            //om vikt
+            Console.WriteLine("skriva din vikt i kg");
+            vikt = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ok din väger " + vikt + ".");
+    }
+}
